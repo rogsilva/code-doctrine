2450d33ba74229cfbd10a686673a506db5dc000f
Rogério Silva <ress.rogerio@gmail.com>
2015-01-07 11:28:30 -0200

    Correção parte 1 do projeto

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/src/Code/Sistema/views',
 ));
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
